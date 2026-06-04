0994168d5def1069522defcfe2f652ce81d96abb
Joi McBrayer <j0ifulnois3@gmail.com>
2026-06-04 14:22:28 -0500

    coding w craig

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,15 +1,37 @@
 package com.pluralsight;
 
 public class Program {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
+
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
         dataSource.setUsername("root");
         dataSource.setPassword("password");
 
+        String sql = "SELECT actor_id,\n" +
+                "    first_name,\n" +
+                "    last_name,\n" +
+                "    last_update\n" +
+                "FROM actor";
 
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
 
+            while (rs.next()) {
+                int actorId = rs.getInt("actor_id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+               Instant lastUpdate = rs.getTimestamp("last_update").toInstant();
+                System.out.printf("%d %s %s %s", actorId, firstName, lastName, lastUpdate.toInstant);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
     }
+
 }
