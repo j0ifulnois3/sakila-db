518ba48ae85d03e0fe19e25de1b5fbfecbbf60f2
Joi McBrayer <j0ifulnois3@gmail.com>
2026-06-04 15:53:52 -0500

    coding w craig;

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -11,37 +11,38 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Program {
-
     public static void main(String[] args) {
-
-
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
         dataSource.setUsername("root");
-        dataSource.setPassword("password");
+        dataSource.setPassword("yearup26");
 
-        String sql = "SELECT actor_id,\n" +
-                "    first_name,\n" +
-                "    last_name,\n" +
-                "    last_update\n" +
-                "FROM actor";
+        String sql = """
+                SELECT actor_id,
+                    first_name,
+                    last_name,
+                    last_update
+                FROM actor;
+                """;
 
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql);
-             ResultSet rs = stmt.executeQuery()) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
 
-            while (rs.next()) {
-                int actorId = rs.getInt("actor_id");
-                String firstName = rs.getString("first_name");
-                String lastName = rs.getString("last_name");
-                Instant lastUpdate = rs.getTimestamp("last_update").toInstant();
-                System.out.printf("%d %s %s %s", actorId, firstName, lastName, lastUpdate.toInstant);
+            while(resultSet.next()){
+                int actorId = resultSet.getInt("actor_id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                Instant lastUpdate = resultSet.getTimestamp("last_update").toInstant();
+                System.out.printf(" %d %s %s %s", actorId, firstName, lastName, lastUpdate.toString());
+                System.out.println();
             }
+
         } catch (SQLException e) {
+            System.out.println("Failed to retrieve actors. Please try again.");
             e.printStackTrace();
         }
 
 
     }
-
 }
