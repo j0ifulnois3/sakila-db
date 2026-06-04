5725df2f163035676debe2c6f0615dbd6a127de8
Joi McBrayer <j0ifulnois3@gmail.com>
2026-06-04 13:15:54 -0500

    coding w craig

---
+++ src/main/java/com/pluralsight/Program.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Program {
+    public static void main(String[] args){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+
+
+
+
+
+    }
+}
